a71ff17a0403cda4daff6111612f5dc091d54ad1
Aditi Prabakaran <adip.2k5@gmail.com>
2024-12-11 21:33:54 +0530

    updated

--- components/BackgroundPattern.tsx
+++ components/BackgroundPattern.tsx
@@ -10,6 +10,7 @@ export function BackgroundPattern() {
         >
           <g clipPath="url(#clip0_1_2)">
             <g opacity="0.4">
+              
               <path
                 d="M400 800C620.914 800 800 620.914 800 400C800 179.086 620.914 0 400 0C179.086 0 0 179.086 0 400C0 620.914 179.086 800 400 800Z"
                 fill="url(#paint0_radial_1_2)"
